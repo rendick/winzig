ae59f1acdd2e632440e6e0d76e823323ba6c3548
Kostiantyn Naddur <rendick123@proton.me>
2026-01-20 20:05:17 +0100

    feat: calculate the ball's angle according to the hitting point

--- winzig.cpp
+++ winzig.cpp
@@ -1,3 +1,10 @@
+#include <hardware/regs/dreq.h>
+#include <pico/stdio.h>
+#include <pico/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
 #include "lib/pico-ssd1306/shapeRenderer/ShapeRenderer.h"
@@ -6,10 +13,6 @@
 #include "pico/binary_info.h"
 #include "pico/stdlib.h"
 #include "textRenderer/5x8_font.h"
-#include <hardware/regs/dreq.h>
-#include <pico/stdio.h>
-#include <pico/time.h>
-#include <stdio.h>
 
 using namespace pico_ssd1306;
 
@@ -28,116 +31,131 @@ using namespace pico_ssd1306;
 
 int score = 0;
 int x1 = X_BALL_POS, x2 = Y_BALL_POS;
-int ball_x = SCREEN_WIDTH / 2, ball_y = 10;
-int ball_speed_x = 1, ball_speed_y = 1;
+float ball_x = SCREEN_WIDTH / 2.0, ball_y = 10.0;
+float ball_speed_x = 1.0, ball_speed_y = 1.0;
 
-void pong_game(pico_ssd1306::SSD1306 &display);
-void menu(pico_ssd1306::SSD1306 &display);
+void pong_game(pico_ssd1306::SSD1306& display);
+void menu(pico_ssd1306::SSD1306& display);
 
-void pong_game(pico_ssd1306::SSD1306 &display) {
-  int status = 1;
+void pong_game(pico_ssd1306::SSD1306& display) {
+	srand(time(NULL));
+	int status = 1;
 
-  while (status) {
-    char score_str[1024];
-    sprintf(score_str, "%d", score);
+	while (status) {
+		char score_str[1024];
+		sprintf(score_str, "%d", score);
 
-    ball_x += ball_speed_x;
-    ball_y += ball_speed_y;
+		ball_x += ball_speed_x;
+		ball_y += ball_speed_y;
+		printf("%fx%f\n", ball_x, ball_y);
+		if(ball_speed_x > 127.0 ){
+			ball_x = (float)SCREEN_WIDTH / 2;
+			ball_y = 10;
+		}
 
-    if (ball_x == 0 || ball_x == SCREEN_WIDTH)
-      ball_speed_x = -ball_speed_x;
+		if ((int)ball_x == 0 || (int)ball_x == SCREEN_WIDTH)
+			ball_speed_x = -ball_speed_x;
 
-    if (ball_y == 0)
-      ball_speed_y = -ball_speed_y;
+		if ((int)ball_y == 0) ball_speed_y = -ball_speed_y;
 
-    if (ball_y == SCREEN_HEIGHT - RACKET_HEIGHT - BALL_SIZE) {
-      if (ball_x >= x1 && ball_x <= x2) {
-        ball_speed_y = -ball_speed_y;
-        score++;
-      } else {
-        score--;
-        ball_x = SCREEN_WIDTH / 2;
-        ball_y = 10;
-      }
-    }
+		if ((int)ball_y == SCREEN_HEIGHT - RACKET_HEIGHT - BALL_SIZE) {
+			if ((int)ball_x >= x1 && (int)ball_x <= x2) {
+				float hit_pos = (float)(ball_x - x1) / (x2 - x1);
+				float offset = hit_pos * 2.0 - 1.0;
 
-    if (gpio_get(R_ARROW) == 0 && x2 <= 127) {
-      x1++, x2++;
-    }
-    if (gpio_get(L_ARROW) == 0 && x1 > 0) {
-      x1--, x2--;
-    }
+				float max_angle = 75.0 *(M_PI / 180.0);
+				float angle = max_angle * offset;
 
-    display.clear();
+				float speed = sqrt(ball_speed_x * ball_speed_x + ball_speed_y * ball_speed_y);
 
-    drawLine(&display, x1, 63, x2, 63);
-    drawLine(&display, 0, 0, 127, 0);
-    drawText(&display, font_5x8, score_str, 10, 10);
+				ball_speed_x = speed * sin(angle);
+				ball_speed_y = -speed * cos(angle);
 
-    display.setPixel(ball_x, ball_y);
+				score++;
+			} else {
+				score--;
+				ball_x = (float)SCREEN_WIDTH / 2;
+				ball_y = 10;
+			}
+		}
 
-    display.sendBuffer();
-    if (gpio_get(MOD_BTN) == 0) {
-      int hold_time = 0;
+		if (gpio_get(R_ARROW) == 0 && x2 <= 127) {
+			x1++, x2++;
+		}
+		if (gpio_get(L_ARROW) == 0 && x1 > 0) {
+			x1--, x2--;
+		}
 
-      while (gpio_get(MOD_BTN) == 0 ) {
-        sleep_ms(10);
-        hold_time += 10;
-        printf("hold time: %d\n", hold_time);
+		display.clear();
 
-        if (hold_time >= 500)
-          break;
-      }
-      if (hold_time >= 500) {
-        status = 0;
-      }
-    }
-  }
+		drawLine(&display, x1, 63, x2, 63);
+		drawLine(&display, 0, 0, 127, 0);
+		drawText(&display, font_5x8, score_str, 10, 10);
 
-  menu(display);
+		display.setPixel(ball_x, ball_y);
 
-  return;
+		display.sendBuffer();
+		if (gpio_get(MOD_BTN) == 0) {
+			int hold_time = 0;
+
+			while (gpio_get(MOD_BTN) == 0) {
+				sleep_ms(10);
+				hold_time += 10;
+				printf("hold time: %d\n", hold_time);
+
+				if (hold_time >= 500) break;
+			}
+			if (hold_time >= 500) {
+				status = 0;
+			}
+		}
+	}
+
+	menu(display);
+
+	return;
 }
 
-void menu(pico_ssd1306::SSD1306 &display) {
-  score = 0;
-  display.clear();
-  drawText(&display, font_5x8, "Press right arrow to start", 0, SCREEN_HEIGHT / 2);
-  display.sendBuffer();
+void menu(pico_ssd1306::SSD1306& display) {
+	score = 0;
+	display.clear();
+	drawText(&display, font_5x8, "Press right arrow to start", 0,
+		 SCREEN_HEIGHT / 2);
+	display.sendBuffer();
 
-  while (gpio_get(R_ARROW) != 0) {
-    sleep_ms(10);
-  }
+	while (gpio_get(R_ARROW) != 0) {
+		sleep_ms(10);
+	}
 
-  pong_game(display);
+	pong_game(display);
 }
 
 int main() {
-  stdio_init_all();
-  sleep_ms(2000);
-  i2c_init(i2c0, 1000000);
-  gpio_set_function(21, GPIO_FUNC_I2C);
-  gpio_set_function(20, GPIO_FUNC_I2C);
-  gpio_pull_up(21);
-  gpio_pull_up(20);
+	stdio_init_all();
+	sleep_ms(2000);
+	i2c_init(i2c0, 1000000);
+	gpio_set_function(21, GPIO_FUNC_I2C);
+	gpio_set_function(20, GPIO_FUNC_I2C);
+	gpio_pull_up(21);
+	gpio_pull_up(20);
 
-  gpio_init(R_ARROW);
-  gpio_set_dir(R_ARROW, GPIO_IN);
-  gpio_pull_up(R_ARROW);
+	gpio_init(R_ARROW);
+	gpio_set_dir(R_ARROW, GPIO_IN);
+	gpio_pull_up(R_ARROW);
 
-  gpio_init(L_ARROW);
-  gpio_set_dir(L_ARROW, GPIO_IN);
-  gpio_pull_up(L_ARROW);
+	gpio_init(L_ARROW);
+	gpio_set_dir(L_ARROW, GPIO_IN);
+	gpio_pull_up(L_ARROW);
 
-  gpio_init(MOD_BTN);
-  gpio_set_dir(MOD_BTN, GPIO_IN);
-  gpio_pull_up(MOD_BTN);
+	gpio_init(MOD_BTN);
+	gpio_set_dir(MOD_BTN, GPIO_IN);
+	gpio_pull_up(MOD_BTN);
 
-  SSD1306 display = SSD1306(i2c0, 0x3C, Size::W128xH64);
+	SSD1306 display = SSD1306(i2c0, 0x3C, Size::W128xH64);
 
-  display.setOrientation(0);
+	display.setOrientation(0);
 
-  while (1) {
-    menu(display);
-  }
+	while (1) {
+		menu(display);
+	}
 }
