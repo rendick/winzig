798028d33ade2e51793839a13fe58dc14f89677b
Kostiantyn Naddur <rendick123@proton.me>
2026-01-26 17:20:47 +0100

    feat: implement SD card support

--- src/winzig.cpp
+++ src/winzig.cpp
@@ -1,7 +1,9 @@
-extern "C"{
-	#include "lauxlib.h"
-	#include "lua.h"
-	#include "lualib.h"
+#include <pico/stdio_usb.h>
+#include <string.h>
+extern "C" {
+#include "lauxlib.h"
+#include "lua.h"
+#include "lualib.h"
 }
 
 #include <hardware/regs/dreq.h>
@@ -11,14 +13,15 @@ extern "C"{
 #include <stdlib.h>
 #include <time.h>
 
+#include "api.h"
+#include "ff.h"
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
+#include "sd_card.h"
 #include "shapeRenderer/ShapeRenderer.h"
 #include "ssd1306.h"
-#include "textRenderer/TextRenderer.h"
 #include "textRenderer/5x8_font.h"
-
-#include "api.h"
+#include "textRenderer/TextRenderer.h"
 
 using namespace pico_ssd1306;
 
@@ -54,7 +57,7 @@ void pong_game(pico_ssd1306::SSD1306& display) {
 		ball_x += ball_speed_x;
 		ball_y += ball_speed_y;
 		printf("%fx%f\n", ball_x, ball_y);
-		if(ball_speed_x > 127.0 ){
+		if (ball_speed_x > 127.0) {
 			ball_x = (float)SCREEN_WIDTH / 2;
 			ball_y = 10;
 		}
@@ -66,13 +69,15 @@ void pong_game(pico_ssd1306::SSD1306& display) {
 
 		if ((int)ball_y == SCREEN_HEIGHT - RACKET_HEIGHT - BALL_SIZE) {
 			if ((int)ball_x >= x1 && (int)ball_x <= x2) {
-				float hit_pos = (float)(ball_x - x1) / (x2 - x1);
+				float hit_pos =
+				    (float)(ball_x - x1) / (x2 - x1);
 				float offset = hit_pos * 2.0 - 1.0;
 
-				float max_angle = 75.0 *(M_PI / 180.0);
+				float max_angle = 75.0 * (M_PI / 180.0);
 				float angle = max_angle * offset;
 
-				float speed = sqrt(ball_speed_x * ball_speed_x + ball_speed_y * ball_speed_y);
+				float speed = sqrt(ball_speed_x * ball_speed_x +
+						   ball_speed_y * ball_speed_y);
 
 				ball_speed_x = speed * sin(angle);
 				ball_speed_y = -speed * cos(angle);
@@ -136,15 +141,108 @@ void menu(pico_ssd1306::SSD1306& display) {
 	pong_game(display);
 }
 
+void parse_dir(){
+	DIR dir;
+	FILINFO fno;
+	FRESULT fr;
+
+	fr = f_opendir(&dir, "0:");
+	if (fr != FR_OK){
+		printf("ERROR: Could not open directory (%d)\n", fr);
+	}
+
+	do {
+		f_readdir(&dir, &fno);
+		if (fno.fname[0] != 0){
+			printf("File: %s\n", fno.fname);
+		}
+	} hile(fno.fname[0] != 0);
+
+	f_closedir(&dir);
+}
+
 int main() {
-	const char *l_test = 
-	"for i = 10, 15, 1\n"
-	"do\n"
-	"draw_pixel(i, 40)\n"
-	"end\n";
+	// const char *l_test =
+	// "for i = 10, 15, 1\n"
+	// "do\n"
+	// "draw_pixel(i, 40)\n"
+	// "end\n";
+
+	char l_test[1024];
+	l_test[0] = '\0';
+
+	FRESULT fr;
+	FATFS fs;
+	FILINFO fno;
+	FIL fil;
+	int ret;
+	char buf[100];
+	char filename[] = "test.txt";
 
 	stdio_init_all();
+
 	sleep_ms(2000);
+	while (!stdio_usb_connected()) {
+		sleep_ms(100);
+	}
+
+	printf("WINZIG\n");
+
+	if (!sd_init_driver()) {
+		printf("ERROR: Could not initialize SD card\r\n");
+	}
+
+
+	fr = f_mount(&fs, "0:", 1);
+	if (fr != FR_OK) {
+		printf("ERROR: Could not mount filesystem (%d)\r\n", fr);
+	}
+
+	parse_dir();
+
+	fr = f_open(&fil, filename, FA_READ);
+	if (fr != FR_OK) {
+		printf("ERROR: Could not open file (%d)\r\n", fr);
+	}
+
+	// ret = f_printf(&fil, "TEST\r\n");
+	// if (ret < 0){
+	// 	printf("ERROR: Could not write to SD card\r\n");
+	// 	f_close(&fil);
+	// }
+	//
+	// ret = f_printf(&fil, "of writing to an SD card.\r\n");
+	// if (ret < 0){
+	// 	printf("ERROR: Could not write to SD card\r\n");
+	// 	f_close(&fil);
+	// }
+
+	fr = f_close(&fil);
+	if (fr != FR_OK) {
+		printf("ERROR: Could not close file (%d)\r\n", fr);
+	}
+
+	fr = f_open(&fil, filename, FA_READ);
+	if (fr != FR_OK) {
+		printf("ERROR: Could not open file (%d)\r\n", fr);
+	}
+
+	printf("Reading from file: %s\n", filename);
+	printf("---\n");
+	while (f_gets(buf, sizeof(buf), &fil)) {
+		strcat(l_test, buf);
+	}
+	printf("\r\n---\r\n");
+
+	printf("LTEST: %s\n", l_test);
+
+	fr = f_close(&fil);
+	if (fr != FR_OK) {
+		printf("ERROR: Could not close file (%d)\r\n", fr);
+	}
+
+	f_unmount("0:");
+
 	i2c_init(i2c0, 1000000);
 	gpio_set_function(21, GPIO_FUNC_I2C);
 	gpio_set_function(20, GPIO_FUNC_I2C);
