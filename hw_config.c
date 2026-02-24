300c320acc295865b060a620c48e7e545ef54a10
Kostiantyn Naddur <rendick123@proton.me>
2026-02-24 18:54:52 +0100

    fix: disable SD card GPIO detector

--- hw_config.c
+++ hw_config.c
@@ -44,8 +44,7 @@ static spi_t spis[] = { // One for each SPI.
         .miso_gpio = 8,  // GPIO number (not pin number)
         .mosi_gpio = 11,
         .sck_gpio = 10,
-        .baud_rate = 1000 * 1000,
-        //.baud_rate = 25 * 1000 * 1000, // Actual frequency: 20833333.
+        .baud_rate = 25 * 1000 * 1000, // Actual frequency: 20833333.
     }};
 
 // Hardware Configuration of the SD Card "objects"
@@ -54,8 +53,8 @@ static sd_card_t sd_cards[] = { // One for each SD card
         .pcName = "0:",  // Name used to mount device
         .spi = &spis[0], // Pointer to the SPI driving this card
         .ss_gpio = 9,    // The SPI slave select GPIO for this SD card
-        .use_card_detect = true,
-        .card_detect_gpio = 13, // Card detect
+        .use_card_detect = false,
+        .card_detect_gpio = -1, // Card detect
         .card_detected_true = 1 // What the GPIO read returns when a card is
                                 // present. Use -1 if there is no card detect.
     }};
